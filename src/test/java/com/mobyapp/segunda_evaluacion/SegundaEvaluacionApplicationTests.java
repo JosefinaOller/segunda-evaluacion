c177764c10d772e2e8072d1f06662497db30d10f
Josefina Oller <josefinaoller19@gmail.com>
2025-10-24 10:20:42 -0300

    First commit

---
+++ src/test/java/com/mobyapp/segunda_evaluacion/SegundaEvaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobyapp.segunda_evaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundaEvaluacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
