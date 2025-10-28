2937b59f6ec911f5d610b48f98ea104a08438ce8
Josefina Oller <josefinaoller19@gmail.com>
2025-10-27 22:43:38 -0300

    Remove redundant semicolon in import statement

--- src/test/java/com/mobyapp/segunda_evaluacion/service/CandidatoServiceTest.java
+++ src/test/java/com/mobyapp/segunda_evaluacion/service/CandidatoServiceTest.java
@@ -12,7 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import static org.junit.jupiter.api.Assertions.*;;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
