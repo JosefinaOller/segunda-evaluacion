8eca2f180ee555ba428f89fe94e1bf39f37e10e9
Josefina Oller <josefinaoller19@gmail.com>
2025-10-28 14:41:17 -0300

    Introduce RecursoNoEncontradoException and Global Handler

---
+++ src/main/java/com/mobyapp/segunda_evaluacion/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.mobyapp.segunda_evaluacion.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(RecursoNoEncontradoException.class)
+    public ResponseEntity<String> handleRecursoNoEncontrado(RecursoNoEncontradoException ex) {
+        log.warn("Petición fallida: Recurso no encontrado. Motivo {}",ex.getMessage());
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+        return errors;
+    }
+
+}
---
+++ src/main/java/com/mobyapp/segunda_evaluacion/exception/RecursoNoEncontradoException.java
@@ -0,0 +1,11 @@
+package com.mobyapp.segunda_evaluacion.exception;
+
+public class RecursoNoEncontradoException extends Exception{
+
+     public RecursoNoEncontradoException(String message){
+        super(message);
+    }
+    public RecursoNoEncontradoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
