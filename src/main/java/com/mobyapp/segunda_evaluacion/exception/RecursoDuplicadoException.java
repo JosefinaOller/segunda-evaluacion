ffc8ad1961181256b696f01f7783074b0a4136cc
Josefina Oller <josefinaoller19@gmail.com>
2025-10-28 22:24:23 -0300

    Add RecursoDuplicadoException

---
+++ src/main/java/com/mobyapp/segunda_evaluacion/exception/RecursoDuplicadoException.java
@@ -0,0 +1,8 @@
+package com.mobyapp.segunda_evaluacion.exception;
+
+public class RecursoDuplicadoException extends Exception {
+
+    public RecursoDuplicadoException(String message){
+        super(message);
+    }
+}
